07f21f8af29fb0145572a831f467a714c8ff3eab
narendrakorrapati <korrapatinarendra843@gmail.com>
2021-05-31 17:08:54 +0530

    branch test alpha

--- UserManagementV1/src/main/java/com/narendra/bss/config/EmailConfig.java
+++ UserManagementV1/src/main/java/com/narendra/bss/config/EmailConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 /**
- * Email configuration file
+ * Email configuration file, changed in alpha branch
  * @author korra
  *
  */
